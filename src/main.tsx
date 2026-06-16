c5c19befaabf94e259ff17dbe75329d64c4183ab
Leelesada <leticiamaya77@gmail.com>
2026-06-16 17:09:26 -0300

    tela preta

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import "./index.css"; // É melhor importar o CSS logo abaixo do React
+import App from "./App"; // Remova o .tsx daqui, o Vite encontra automaticamente
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
