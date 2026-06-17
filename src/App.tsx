422bd526164f1f9e8c5081367358e66892ef6f61
Leelesada <leticiamaya77@gmail.com>
2026-06-17 09:21:10 -0300

    jesus

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Footer from "./components/Footer";
-import { motion, AnimatePresence } from "motion/react";
-
 export default function App() {
   return (
     <AnimatePresence>
       <motion.div 
-  className="min-h-screen bg-black text-white font-sans overflow-x-hidden flex flex-col items-center"
-  id="landing-page-container"
->
+        className="min-h-screen bg-black text-white font-sans overflow-x-hidden flex flex-col items-center"
+        id="landing-page-container"
+      >
+        {/* Adicione as chamadas de volta aqui dentro: */}
+        <Header />
+        <Hero />
+        <Footer />
       </motion.div>
     </AnimatePresence>
   );
